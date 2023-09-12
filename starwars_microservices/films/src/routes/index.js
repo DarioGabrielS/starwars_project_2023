5940e8f8a0f3edcab47cc95e6a358c19bb90e7d3
DarioGabrielS <dariogabriel.salvadore@gmail.com>
2023-09-12 19:12:04 -0300

    Configuracion de server y routes, middleware validador, servicio Films completo al dia de la fecha

--- starwars_microservices/films/src/routes/index.js
+++ starwars_microservices/films/src/routes/index.js
@@ -1,8 +1,11 @@
 const {Router} = require('express')
 const controllers = require('../controllers')
+const middlewares = require('../middlewares')
 
 const router = Router()
 
-router.get('/', controllers.getFilms)
+router.get('/', middlewares.nameValidator, controllers.getFilms)
+// En esta ruta get no tiene sentido real poner un middleware validador. Es mas
+// va a dar error siempre porque espera un parametro "name" para poder ingresar a la ruta
 
 module.exports = router
--- starwars_microservices/films/src/server.js
+++ starwars_microservices/films/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = require('./routes/index')
 const morgan = require('morgan')
+
 const server = express()
 
 server.use(morgan("dev"))
@@ -8,4 +9,14 @@ server.use(express.json())
 
 server.use(router)
 
+server.use('*', (req,res)=>{
+    res.status(404).send('Not found')
+})
+
+server.use((err,req,res,next)=>{
+    res.status(err.statusCode || 500).send({
+        error: true,
+        message: err.message
+    })
+})
 module.exports= server
