5b167cf3ed522cd8003b10098f0ebd7e51a9cbb9
DarioGabrielS <dariogabriel.salvadore@gmail.com>
2023-09-19 19:44:27 -0300

    Creacion planetSchema y filmSchema

--- starwars_microservices/dataMicroService/src/database/schemas/planetSchema.js
+++ starwars_microservices/dataMicroService/src/database/schemas/planetSchema.js
@@ -19,18 +19,5 @@ const planetSchema = new Schema({
     }]
 
 })
-/*
-{
-    "_id": "35",
-    "name": "Malastare",
-    "rotation_period": "26",
-    "orbital_period": "201",
-    "diameter": "18880",
-    "climate": "arid, temperate, tropical",
-    "gravity": "1.56",
-    "terrain": "swamps, deserts, jungles, mountains",
-    "surface_water": "unknown",
-    "residents": ["41"],
-    "films": []
-  },
-*/
+
+module.exports= planetSchema
