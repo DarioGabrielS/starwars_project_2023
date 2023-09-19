c94607b0d1a5cc84cac6a593544504bf82ee2eb2
DarioGabrielS <dariogabriel.salvadore@gmail.com>
2023-09-19 14:09:46 -0300

    config dotenv, definicion characterSchema

---
+++ starwars_microservices/dataMicroService/src/database/schemas/character.js
@@ -0,0 +1,35 @@
+const {Schema} = require('mongoose')
+
+const characterSchema = new Schema({
+    _id: {
+        type: String,
+        required: true,
+        unique:true
+    },
+    name:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    height: String,
+    mass: String,
+    hair_color: String,
+    skin_color: String,
+    eye_color: String,
+    birth_year: String,
+    gender:{
+        type: String,
+        enum: ['male','female','none','hermaphrodite','n/a','other']
+
+    },
+    homeworld:{
+        type:String,
+        ref:'Planet'
+    },
+    films:[{
+        type: String,
+        ref: 'Film'
+    }]
+})
+
+module.exports= characterSchema
--- starwars_microservices/dataMicroService/src/server.js
+++ starwars_microservices/dataMicroService/src/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const cors = require('cors')
-
+const morgan = require('morgan')
 
 const server = express()
-server.use(express.json)
+server.use(express.json())
+server.use(morgan("dev"))
 server.use(cors)
 
 
